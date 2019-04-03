aeccc73ad482dc75272bb1c4448d109eb8089560
Viktor Sidochenko <viktor.sidochenko@gmail.com>
2019-04-04 00:39:37 +0700

    add vesrion query string add github buttons

--- src/app.js
+++ src/app.js
@@ -1,3 +1,44 @@
+function UpdateQueryString(key, value, url) {
+    if (!url) url = window.location.href;
+    var re = new RegExp("([?&])" + key + "=.*?(&|#|$)(.*)", "gi"),
+        hash;
+
+    if (re.test(url)) {
+        if (typeof value !== 'undefined' && value !== null)
+            return url.replace(re, '$1' + key + "=" + value + '$2$3');
+        else {
+            hash = url.split('#');
+            url = hash[0].replace(re, '$1$3').replace(/(&|\?)$/, '');
+            if (typeof hash[1] !== 'undefined' && hash[1] !== null) 
+                url += '#' + hash[1];
+            return url;
+        }
+    }
+    else {
+        if (typeof value !== 'undefined' && value !== null) {
+            var separator = url.indexOf('?') !== -1 ? '&' : '?';
+            hash = url.split('#');
+            url = hash[0] + separator + key + '=' + value;
+            if (typeof hash[1] !== 'undefined' && hash[1] !== null) 
+                url += '#' + hash[1];
+            return url;
+        }
+        else
+            return url;
+    }
+}
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+
 function iframeHash() {
     window.location.hash = document.getElementById('doc').contentWindow.location.hash;
 }
@@ -11,6 +52,9 @@ function iframeHashListener() {
 
 function setVersion(version) {
     document.title = "TAG documentation (" + version + ")";
+    if(getParameterByName('version') !== version) {
+        window.location= UpdateQueryString('version', version);
+    }
     $('#doc').attr("src", "releases/" + version + "/index.html" + window.location.hash);
 }
 
@@ -28,10 +72,12 @@ $.getJSON("https://api.github.com/repositories/172893709/contents/releases", fun
     $('#versions')
     .append(options.map(function (version) { return "<option value=" + version + ">" + version + "</option>" }).join(""));
     
-    const latestRelease = options.length > 1 ? options[options.length - 2] : "snapshot";
-    
-    $('#versions').val(latestRelease);
+    const latestRelease = options.length > 1 ? options[options.length - 2] : "snapshot";  
+    const queryVersion = getParameterByName('version');  
+    const version = queryVersion && options.indexOf(queryVersion) ? queryVersion : latestRelease;
+
+    $('#versions').val(version);
     $('#versions').selectmenu("refresh");
-    setVersion(latestRelease);
+    setVersion(version);
     window.location.hash = "";
 });
